c2aafbc069e0f840f4b3c06a81797898656275fd
Ledyanoy <ledyanoy44@gmail.com>
2019-06-30 03:57:47 +0300

    gulp add

---
+++ app/js/script.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    var btn = document.querySelector('.btn');    
+
+    btn.addEventListener('click', function(item) {
+        item.preventDefault();
+        ajaxGet();
+    });
+}
+var cityListEl = document.querySelector('.city-list');
+
+// clar old city list 
+function clear () {  
+    cityListEl.innerHTML= '';    
+}
+
+// create city list 
+function render (item) {      
+    var div = document.createElement('div');
+    div.className = 'city';
+    div.innerHTML = `<strong class="cite__name">${item.city_name}</strong> 
+                    <img class="cite__picture" src="${item.image_url}"/>`;     
+    cityListEl.appendChild(div);
+}
+
+function renderOther (item) {
+    var city = document.querySelector('.cityother');
+    
+    city.innerHTML += `<div><h2>${item.city_name}</h2></div>`;
+}
+
+
+function ajaxGet () {
+    var xhr = new XMLHttpRequest (); 
+    xhr.onreadystatechange =  function() {
+       if(xhr.readyState == 4 && xhr.status == 200) {
+        clear();
+        var locations = JSON.parse(xhr.response);         
+        locations.forEach(function (location) {
+            if(location.country =="Russian Federation") {            
+            render(location);
+            } else if (location.country =="United States of America") {
+                renderOther(location);
+            }
+        });
+       }
+    };
+    xhr.open('GET', "../locations.json");
+    xhr.setRequestHeader("X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com");
+    xhr.setRequestHeader("X-RapidAPI-Key", "1b0e442584msh4cd153768d0eec3p1dc04cjsn664d2bd185ed");
+    xhr.send();
+}
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp');
+    browserSync = require('browser-sync');
+    function reload() {
+        browserSync.reload();
+        done();
+      };            
+    pug = require('gulp-pug');
+    sass = require('gulp-sass');
+    autoprefixer = require('gulp-autoprefixer');
+    imagemin     = require('gulp-imagemin'),
+    imgCompress  = require('imagemin-jpeg-recompress');
+    newer = require("gulp-newer");
+    
+      
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dist/'
+        },
+        notify: false
+    });    
+});
+
+gulp.task('pug', function() {
+    return gulp.src('app/pug/index.pug')
+      .pipe(pug({
+          pretty: true
+        }))
+      .pipe(gulp.dest('dist/'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.stream());     
+});
+
+gulp.task('autoprefixer', function () {
+  return gulp.src('dist/styles.css')
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*.{jpg,jpeg,png,gif}')   
+    .pipe(newer("dist/img")) 
+    .pipe(imagemin([
+      imgCompress({
+        loops: 4,
+        min: 70,
+        max: 80,
+        quality: 'high'
+      }),
+      imagemin.gifsicle(),
+      imagemin.optipng(),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/pug/**/*.pug', gulp.parallel('pug'));
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/img/**/*', gulp.parallel('img'));
+    gulp.watch('app/js/**/*.js', reload);
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
