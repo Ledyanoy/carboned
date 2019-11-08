5b1eb128978f20bca2a3a066b5912244e2852aaa
Ledyanoy <ledyanoy44@gmail.com>
2019-11-09 00:09:04 +0400

    add footer

--- dist/script.js
+++ dist/script.js
@@ -1,85 +1,8 @@
-// window.onload = function() {
-//     var btn = document.querySelector('.btn');    
-
-//     btn.addEventListener('click', function(item) {
-//         item.preventDefault();
-//         ajaxGet();
-//     });
-// }
-// var cityListEl = document.querySelector('.city-list');
-
-// // clar old city list 
-// function clear () {  
-//     cityListEl.innerHTML= '';    
-// }
-
-// // create city list 
-// function render (item) {      
-//     var div = document.createElement('div');
-//     div.className = 'city';
-//     div.innerHTML = `<strong class="cite__name">${item.city_name}</strong> 
-//                     <img class="cite__picture" src="${item.image_url}"/>`;     
-//     cityListEl.appendChild(div);
-// }
-
-// function renderOther (item) {
-//     var city = document.querySelector('.cityother');
-
-//     city.innerHTML += `<div><h2>${item.city_name}</h2></div>`;
-// }
-
-
-// function ajaxGet () {
-//     var xhr = new XMLHttpRequest (); 
-//     xhr.onreadystatechange =  function() {
-//        if(xhr.readyState == 4 && xhr.status == 200) {
-//         clear();
-//         var locations = JSON.parse(xhr.response);         
-//         locations.forEach(function (location) {
-//             if(location.country =="Russian Federation") {            
-//             render(location);
-//             } else if (location.country =="United States of America") {
-//                 renderOther(location);
-//             }
-//         });
-//        }
-//     };
-//     xhr.open('GET', "../locations.json");
-//     xhr.setRequestHeader("X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com");
-//     xhr.setRequestHeader("X-RapidAPI-Key", "1b0e442584msh4cd153768d0eec3p1dc04cjsn664d2bd185ed");
-//     xhr.send();
-// }
-console.log('работает script.js');
-var newScript = document.createElement("script");
-newScript.src = "main.js";
-document.body.appendChild(newScript);
-
 var lamboList = document.querySelector('.main-lambo-list');
 
 var lamboListItems = document.querySelectorAll('.main-lambo-list__item');
 
-// lamboListItems.forEach((elem) => {
 
-//     elem.addEventListener('mouseover', (evt) => {
-
-//         lamboListItems.forEach((item) => {
-//             item.classList.remove('unhovered');
-//             if (item !== evt.target.closest('.main-lambo-list__item')) {
-//                 item.classList.add('unhovered')
-//             }
-//         });
-//         // evt.target.closest('.main-lambo-list__item').classList.remove('vot');
-//     });
-
-//     elem.addEventListener('mouseout', () => {
-
-//         lamboListItems.forEach((item) => {
-//             item.classList.remove('unhovered');
-//         });
-//         // evt.target.closest('.main-lambo-list__item').classList.remove('vot');
-//     });
-
-// });
 
 var onHover = function (evt) {
     if (!evt.target.closest('.main-lambo-list__item')) return;
@@ -106,18 +29,18 @@ lamboList.removeEventListener('mouseover', outHover);
 lamboList.removeEventListener('mouseout', onHover);
 lamboList.addEventListener('mouseout', outHover);
 
-
+// swiper starts
 var galleryThumbs = new Swiper('.gallery-thumbs', {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     autoHeight: true,
     watchSlidesVisibility: true,
-    watchSlidesProgress: true,
+    watchSlidesProgress: true,    
 });
 var galleryTop = new Swiper('.gallery-top', {
     spaceBetween: 10,
-    autoHeight: true,
+    autoHeight: true,    
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -126,27 +49,17 @@ var galleryTop = new Swiper('.gallery-top', {
         swiper: galleryThumbs,
     },
 });
+// swiper ends
 
-
-
-
-// lamboListItems.forEach((elem) => {
-
-//     elem.addEventListener('mouseover', (evt) => {
-
-//         lamboListItems.forEach((item) => {
-//             item.classList.remove('unhovered');
-//             if (item !== evt.target.closest('.main-lambo-list__item')) {
-//                 item.classList.add('unhovered')
-//             }
-//         });
-//         // evt.target.closest('.main-lambo-list__item').classList.remove('vot');
-//     });
-
-//     elem.addEventListener('mouseout', () => {
-
-//         lamboListItems.forEach((item) => {
-//             item.classList.remove('unhovered');
-//         });
-//         // evt.target.closest('.main-lambo-list__item').classList.remove('vot');
-//     });
+// anchor links working start
+var anchors = document.querySelectorAll(".item-question");
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener("click", function (evt) {
+      evt.preventDefault();      
+      var blockID = anchor.getAttribute("href").substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+// anchor links working end
